f69d9a9223651bd8c2f8b40effa64e80cc4c06e1
Itamar Ostricher <itamarost@gmail.com>
2022-10-31 09:20:10 -0700

    Enable OSS strict module test

--- oss-cinder-test.sh
+++ oss-cinder-test.sh
@@ -9,4 +9,4 @@ env
 # make -j 4 VERBOSE=1 testcinder_jit
 # make -j 4 VERBOSE=1 testruntime
 make -j 4 VERBOSE=1 testcinder
-# make -j 4 VERBOSE=1 test_strict_module
+make -j 4 VERBOSE=1 test_strict_module
